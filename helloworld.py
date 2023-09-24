29ce0ea6dd6305e05724f9dcae62e8671b24d58e
eesha-d <eesha-d@users.noreply.github.com>
2023-09-24 00:55:33 -0400

    Part 3: Used bootstrap to implement a navigation bar, format the text and page, and add timestamp information

--- helloworld.py
+++ helloworld.py
@@ -1,15 +1,25 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 #Create flask instance
 app = Flask(__name__)
 
+#Create bootstrap instance
+bootstrap = Bootstrap(app)
+
+#Create moment instance
+moment = Moment(app)
+
 @app.route('/')
 def hello_world():
-	return '<H1>Hello world!</H1>'
+	return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
-	return '<H1>Hello, {}!</H1>'.format(name)
+	return render_template('user.html', name=name)
+
+
 
 if __name__ == '__main__':
 	app.run()
