a16c919ae13261448f392da801a2e13da71dd01e
eesha-d <eesha-d@users.noreply.github.com>
2023-09-24 16:58:31 -0400

    Part 4: Implemented form for user to enter their name, and upon submission, personalizes the hello message

--- helloworld.py
+++ helloworld.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
 #Create flask instance
 app = Flask(__name__)
 
@@ -11,9 +16,21 @@ bootstrap = Bootstrap(app)
 #Create moment instance
 moment = Moment(app)
 
-@app.route('/')
+app.config['SECRET_KEY'] = 'string'
+
+class NameForm(FlaskForm):
+	name = StringField('What is your name?', validators=[DataRequired()])
+	submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-	return render_template('index.html', current_time=datetime.utcnow())
+	name=None
+	form = NameForm()
+
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.html', current_time=datetime.utcnow(), form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
